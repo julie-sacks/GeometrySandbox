e32bcb2867aa4d56d8ebad8f9824ad9538e28151
julie <juliesacksdev@gmail.com>
2024-04-02 16:17:15 -0700

    Basic window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(void)
 {
     printf("Hello world!\n");
+
+    if(!glfwInit()) return 1;
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Geometry", NULL, NULL);
+    if(!window)
+    {
+        printf("couldn't create window\n");
+        return 1;
+    }
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
